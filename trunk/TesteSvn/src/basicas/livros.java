a6058c63732ee6517e15b95db81f685249ea1755
paulo.roger87@gmail.com <paulo.roger87@gmail.com@50527d20-a913-11de-892d-7b2b76e7707e>
2009-09-24 17:17:00 +0000

    Modificada a classe livros.

--- trunk/TesteSvn/src/basicas/livros.java
+++ trunk/TesteSvn/src/basicas/livros.java
@@ -5,5 +5,24 @@ public class livros {
 	private String nome;
 	private int páginas;
 	private String autor;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getPáginas() {
+		return páginas;
+	}
+	public void setPáginas(int páginas) {
+		this.páginas = páginas;
+	}
+	public String getAutor() {
+		return autor;
+	}
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
 
+	
 }
