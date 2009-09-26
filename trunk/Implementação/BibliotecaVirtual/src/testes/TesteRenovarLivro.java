e015a7a6b4a764965c96c2a382f8dcd3a6ef5cb8
paulo.roger87@gmail.com <paulo.roger87@gmail.com@50527d20-a913-11de-892d-7b2b76e7707e>
2009-09-26 12:42:37 +0000

    Inserido os testes da atividade de renovarLivro(); só os cabeçalhos.

---
+++ trunk/Implementação/BibliotecaVirtual/src/testes/TesteRenovarLivro.java
@@ -0,0 +1,33 @@
+package testes;
+
+import junit.framework.TestCase;
+
+public class TesteRenovarLivro extends TestCase {
+	
+	
+	
+	public void testLivroEstaAtrasadoVerdadeiro(){
+		
+	}
+	
+	public void testLivroEstaAtrasadoFalso(){
+		
+	}
+	
+	public void testPossuiFilaEsperaVerdadeiro(){
+		
+	}
+	
+	public void testPossuiFilaEsperaFalso(){
+		
+	}
+	
+	public void testBuscarMeusExemplaresOk(){
+		
+	}
+	
+	public void bu
+	
+	
+
+}
