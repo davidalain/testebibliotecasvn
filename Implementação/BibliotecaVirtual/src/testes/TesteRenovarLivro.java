7843f8eaa7a5df9c6f12504c95056c9d4f76f5bb
paulo.roger87@gmail.com <paulo.roger87@gmail.com@50527d20-a913-11de-892d-7b2b76e7707e>
2009-09-26 14:57:22 +0000

    Implementados alguns métodos de teste da história renovar livro. (David e Roger)

--- Implementação/BibliotecaVirtual/src/testes/TesteRenovarLivro.java
+++ Implementação/BibliotecaVirtual/src/testes/TesteRenovarLivro.java
@@ -1,5 +1,7 @@
 package testes;
 
+import java.util.List;
+
 import junit.framework.TestCase;
 
 public class TesteRenovarLivro extends TestCase {
@@ -15,18 +17,63 @@ public class TesteRenovarLivro extends TestCase {
 	}
 	
 	public void testPossuiFilaEsperaVerdadeiro(){
-		
+	
+		Livro livroAlvo = new Livro();
+		Aluno alunoAlvo = new Aluno();
+		livroAlvo.setListaEspera(new List(alunoAlvo));
+		assertTrue("Existe lista de espera não vazia", possuiFilaEspera(livroAlvo).size()==1);
 	}
 	
 	public void testPossuiFilaEsperaFalso(){
-		
+		Livro livroAlvo = new Livro();
+		Aluno alunoAlvo = new Aluno();
+		List listaVazia = new List(alunoAlvo);
+		listaVazia.remove(alunoAlvo);
+		livroAlvo.setListaEspera(listaVazia);
+		assertTrue("Não existe lista de espera", possuiFilaEspera(livroAlvo).size ==0);
+
 	}
 	
 	public void testBuscarMeusExemplaresOk(){
+		Aluno alunoAlvo = new Aluno();
+		Exemplar exemplarAlvo1 = new Exemplar();
+		Exemplar exemplarAlvo2 = new Exemplar();
+		controlador.alugar(alunoAlvo,exemplarAlvo1);
+		controlador.alugar(alunoAlvo,exemplarAlvo2);
+		assertTrue("Exemplar1 alugado", buscarMeusExemplares(alunoAlvo).size()==2);
 		
 	}
 	
-	public void bu
+	public void testBuscarMeusExemplaresVazio(){
+		Aluno alunoAlvo = new Aluno();
+		Exemplar exemplarAlvo1 = new Exemplar();
+		Exemplar exemplarAlvo2 = new Exemplar();
+		assertFalse("Não terá exemplar alugado", buscarMeusExemplares(alunoAlvo).size()==0);
+	}
+	
+	public void testRenovarExemplarPossivel(){
+		Aluno alunoAlvo = new Aluno();
+		Exemplar exemplarAlvo = new Exemplar();
+		aluno.setDivida(0);
+		controlador.alugar(alunoAlvo,exemplarAlvo);
+		assertTrue("Buscar meus exemplares contem o livro que eu quero renovar", buscarMeusExemplares(alunoAlvo).contains(exemplarAlvo));
+		assertTrue("Checar a situação financeira do aluno", validarSituacaoFinanceira(alunoAlvo) == 0);
+		assertFalse("Chechar se o livro não está na fila de espera",possuiFilaEspera(exemplarAlvo));
+		assertTrue("Livro renovado com sucesso", renovarExemplar(alunoAlvo,exemplarAlvo));
+		
+	}
+	public void testRenovarExemplarNaoPossivel(){
+		
+		Aluno alunoAlvo = new Aluno();
+		Exemplar exemplarAlvo = new Exemplar();
+		Exemplar exemplarNaoAlvo = new Exemplar();
+		aluno.setDivida(2);
+		controlador.alugar(alunoAlvo,exemplarNaoAlvo);
+		assertFalse("Buscar meus exemplares não contem o livro que eu quero renovar", buscarMeusExemplares(alunoAlvo).contains(exemplarAlvo));
+		assertFalse("Checar a situação financeira do aluno", validarSituacaoFinanceira(alunoAlvo) == 0);
+		assertTrue("Chechar se o livro está na fila de espera",possuiFilaEspera(exemplarAlvo));
+		assertFalse("Livro não pode ser renovado", renovarExemplar(alunoAlvo,exemplarAlvo));
+	}
 	
 	
 
