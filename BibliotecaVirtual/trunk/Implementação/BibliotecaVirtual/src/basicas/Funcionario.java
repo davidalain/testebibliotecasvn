63e9591c24ee5761921887eb2887222076c61127
barboza.erick@gmail.com <barboza.erick@gmail.com@50527d20-a913-11de-892d-7b2b76e7707e>
2009-10-08 20:10:45 +0000

    exempla e funcionario criadas

--- BibliotecaVirtual/trunk/Implementação/BibliotecaVirtual/src/basicas/Funcionario.java
+++ BibliotecaVirtual/trunk/Implementação/BibliotecaVirtual/src/basicas/Funcionario.java
@@ -1,5 +1,28 @@
 package basicas;
 
 public class Funcionario {
-
+	private String nome;
+	private String cpf;
+	private String senha;
+	
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	
+	
 }
