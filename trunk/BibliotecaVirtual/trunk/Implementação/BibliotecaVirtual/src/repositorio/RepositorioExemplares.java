dc7006bc4c0aecb8aac25fe0f31183021253304b
barboza.erick@gmail.com <barboza.erick@gmail.com@50527d20-a913-11de-892d-7b2b76e7707e>
2009-10-08 20:56:10 +0000

    repositorio exemplares e funcionarios criados

---
+++ trunk/BibliotecaVirtual/trunk/Implementação/BibliotecaVirtual/src/repositorio/RepositorioExemplares.java
@@ -0,0 +1,31 @@
+package repositorio;
+
+import basicas.Exemplar;
+
+public class RepositorioExemplares {
+	private BancoDados bancoDados;
+	
+	public RepositorioExemplares()
+	{
+		
+	}
+	
+	public void inserir (Exemplar exemplar){
+		bancoDados.inserir(exemplar);
+		
+	}
+	
+	public void remover (Exemplar exemplar){
+		bancoDados.remover(exemplar);
+	}
+	
+	public Exemplar buscar (Exemplar exemplar){
+		return bancoDados.buscar(exemplar);
+		
+	}
+	
+	public void atualizar (Exemplar exemplar){
+		bancoDados.atualizar(exemplar);
+		
+	}
+}
