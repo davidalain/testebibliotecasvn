bc17abac2affd5c06b0489ded97b8ad7675b11d1
barboza.erick@gmail.com <barboza.erick@gmail.com@50527d20-a913-11de-892d-7b2b76e7707e>
2009-10-08 20:38:46 +0000

    RepositorioFuncionarios completo

--- trunk/BibliotecaVirtual/trunk/Implementação/BibliotecaVirtual/src/repositorio/RepositorioFuncionarios.java
+++ trunk/BibliotecaVirtual/trunk/Implementação/BibliotecaVirtual/src/repositorio/RepositorioFuncionarios.java
@@ -1,13 +1,28 @@
 package repositorio;
 
+import basicas.Aluno;
 import basicas.Funcionario;
 
 public class RepositorioFuncionarios {
 	private BancoDados bancoDados;
 	
-	public void inserir(Funcionario f)
-	{
-		bancoDados.inserir(f);
+	public void inserir (Funcionario funcionario){
+		bancoDados.inserir(funcionario);
+		
+	}
+	
+	public void remover (Funcionario funcionario){
+		bancoDados.remover(funcionario);
+	}
+	
+	public Aluno buscar (Funcionario funcionario){
+		return bancoDados.buscar(funcionario);
+		
+	}
+	
+	public void atualizar (Funcionario funcionario){
+		bancoDados.atualizar(funcionario);
+		
 	}
 	
 }
