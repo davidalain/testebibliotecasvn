7975f1c99b873e7b1ec9feec7d9ab0025cbe0d57
barboza.erick@gmail.com <barboza.erick@gmail.com@50527d20-a913-11de-892d-7b2b76e7707e>
2009-10-08 22:30:38 +0000

    fachada vazia

--- BibliotecaVirtual/trunk/Implementação/BibliotecaVirtual/src/fachada/Fachada.java
+++ BibliotecaVirtual/trunk/Implementação/BibliotecaVirtual/src/fachada/Fachada.java
@@ -1,23 +1,6 @@
 package fachada;
 
-import basicas.*;
-import controlador.*;
-import exceptions.*;
 
 public class Fachada {
-	private ControladorAluno ctrlAlunos;
-	private ControladorExemplares ctrlExemplares;
-	
-	public void Alugar(Aluno a, Exemplar e) throws AlunoNaoCadastradoException, AlunoDevendoException
-	{
-		if(!ctrlAlunos.validarSituacaoCadastral(a))
-			throw new AlunoNaoCadastradoException();
-		
-		if(!ctrlAlunos.validarSituacaoFinanceira(a))
-			throw new AlunoDevendoException(a.getDivida());
-		
-		if()
-	}
-	
-	
+
 }
