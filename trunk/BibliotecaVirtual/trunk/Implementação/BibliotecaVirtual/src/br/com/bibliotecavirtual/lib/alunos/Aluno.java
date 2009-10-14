bf1500f1f50f88cbe2e49727ce99c3ce9af68aff
barboza.erick@gmail.com <barboza.erick@gmail.com@50527d20-a913-11de-892d-7b2b76e7707e>
2009-10-14 19:38:39 +0000

    inserir criada

--- trunk/BibliotecaVirtual/trunk/Implementação/BibliotecaVirtual/src/br/com/bibliotecavirtual/lib/alunos/Aluno.java
+++ trunk/BibliotecaVirtual/trunk/Implementação/BibliotecaVirtual/src/br/com/bibliotecavirtual/lib/alunos/Aluno.java
@@ -1,6 +1,5 @@
 package br.com.bibliotecavirtual.lib.alunos;
 
-
 public class Aluno {
 
 	private String nome;
@@ -8,10 +7,9 @@ public class Aluno {
 	private String senha;
 	private String telefone;
 	private String email;
-	
-	
-	public Aluno(String nome, String cpf, String senha,
-			String telefone, String email) {
+
+	public Aluno(String nome, String cpf, String senha, String telefone,
+			String email) {
 		super();
 		this.nome = nome;
 		this.cpf = cpf;
@@ -19,38 +17,45 @@ public class Aluno {
 		this.telefone = telefone;
 		this.email = email;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	public String getCpf() {
 		return cpf;
 	}
+
 	public void setCpf(String cpf) {
 		this.cpf = cpf;
 	}
+
 	public String getSenha() {
 		return senha;
 	}
+
 	public void setSenha(String senha) {
 		this.senha = senha;
 	}
+
 	public String getTelefone() {
 		return telefone;
 	}
+
 	public void setTelefone(String telefone) {
 		this.telefone = telefone;
 	}
-	
-	
-	
+
 }
