b0ae0fae467fa51f6f6209970bda1dc95083b932
barboza.erick@gmail.com <barboza.erick@gmail.com@50527d20-a913-11de-892d-7b2b76e7707e>
2009-10-15 16:06:46 +0000

    exemplar herdando de livro

--- BibliotecaVirtual/trunk/Implementação/BibliotecaVirtual/src/br/com/bibliotecavirtual/lib/exemplares/Exemplar.java
+++ BibliotecaVirtual/trunk/Implementação/BibliotecaVirtual/src/br/com/bibliotecavirtual/lib/exemplares/Exemplar.java
@@ -1,45 +1,14 @@
 package br.com.bibliotecavirtual.lib.exemplares;
 
-public class Exemplar {
-	private String titulo;
-	private String autor;
-	private String isbn;
-	private String area;
-	private String ano;
+import bir.com.bibliotecavirtual.lib.livros.Livro;
+
+public class Exemplar extends Livro {
 	private int idExemplar;
-	public void setTitulo(String titulo) {
-		this.titulo = titulo;
-	}
-	public String getTitulo() {
-		return titulo;
-	}
-	public void setAutor(String autor) {
-		this.autor = autor;
-	}
-	public String getAutor() {
-		return autor;
-	}
-	public void setIsbn(String isbn) {
-		this.isbn = isbn;
-	}
-	public String getIsbn() {
-		return isbn;
-	}
-	public void setArea(String area) {
-		this.area = area;
-	}
-	public String getArea() {
-		return area;
-	}
-	public void setAno(String ano) {
-		this.ano = ano;
-	}
-	public String getAno() {
-		return ano;
-	}
+
 	public void setIdExemplar(int idExemplar) {
 		this.idExemplar = idExemplar;
 	}
+
 	public int getIdExemplar() {
 		return idExemplar;
 	}
