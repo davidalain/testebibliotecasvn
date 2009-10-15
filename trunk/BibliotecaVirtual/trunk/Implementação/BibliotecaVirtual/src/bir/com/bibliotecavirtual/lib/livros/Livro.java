6ada27e6ad34a32a737cff2c6f36dcacfcec11b6
barboza.erick@gmail.com <barboza.erick@gmail.com@50527d20-a913-11de-892d-7b2b76e7707e>
2009-10-15 16:52:43 +0000

    classe livro

---
+++ trunk/BibliotecaVirtual/trunk/Implementação/BibliotecaVirtual/src/bir/com/bibliotecavirtual/lib/livros/Livro.java
@@ -0,0 +1,58 @@
+package bir.com.bibliotecavirtual.lib.livros;
+
+public class Livro {
+	private String titulo;
+	private String autor;
+	private String isbn;
+	private String area;
+	private String ano;
+
+	public Livro(String titulo, String autor, String isbn, String area,
+			String ano) {
+		this.titulo = titulo;
+		this.autor = autor;
+		this.isbn = isbn;
+		this.area = area;
+		this.ano = ano;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+
+	public String getAutor() {
+		return autor;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setArea(String area) {
+		this.area = area;
+	}
+
+	public String getArea() {
+		return area;
+	}
+
+	public void setAno(String ano) {
+		this.ano = ano;
+	}
+
+	public String getAno() {
+		return ano;
+	}
+}
