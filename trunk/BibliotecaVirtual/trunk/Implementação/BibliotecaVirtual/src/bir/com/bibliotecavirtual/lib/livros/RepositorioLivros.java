42df7730c7e15f62b73c2b1239e68f5909955ae4
barboza.erick@gmail.com <barboza.erick@gmail.com@50527d20-a913-11de-892d-7b2b76e7707e>
2009-10-15 17:16:05 +0000

    controlador e repositorio livros

---
+++ trunk/BibliotecaVirtual/trunk/Implementação/BibliotecaVirtual/src/bir/com/bibliotecavirtual/lib/livros/RepositorioLivros.java
@@ -0,0 +1,75 @@
+package bir.com.bibliotecavirtual.lib.livros;
+
+import java.sql.ResultSet;
+
+import br.com.bibliotecavirtual.dados.Conexao;
+
+public class RepositorioLivros {
+	private Conexao conexao = new Conexao();
+
+	public RepositorioLivros() {
+		conexao = new Conexao();
+	}
+
+	private void executarNonQuery(String sql) {
+		conexao.abrirConexao();
+		conexao.executarNonQuery(sql);
+		conexao.fecharConexao();
+	}
+
+	public void inserir(Livro livro) {
+
+		String titulo = livro.getTitulo();
+		String autor = livro.getAutor();
+		String area = livro.getArea();
+		String ano = livro.getAno();
+		String isbn = livro.getIsbn();
+
+		String sql = "INSERT INTO [biblioteca].[dbo].[LIV_LIVRO]"
+				+ "([LIV_NM_TITULO],[LIV_NM_AUTOR],[LIV_SC_AREA],[LIV_NU_ANO],[LIV_NM_ISBN])"
+				+ " VALUES('" + titulo + "','" + autor + "','" + area + "','"
+				+ ano + "','" + isbn + "')";
+
+		this.executarNonQuery(sql);
+	}
+
+	public void remover(Livro livro) {
+		String isbn = livro.getIsbn();
+
+		String sql = "DELETE FROM [biblioteca].[dbo].[LIV_LIVRO]"
+				+ "WHERE LIV_NM_ISBN = '" + isbn + "'";
+
+		this.executarNonQuery(sql);
+	}
+
+	public void atualizar(Livro livro) {
+		String titulo = livro.getTitulo();
+		String autor = livro.getAutor();
+		String area = livro.getArea();
+		String ano = livro.getAno();
+		String isbn = livro.getIsbn();
+
+		String sql = "UPDATE [biblioteca].[dbo].[LIV_LIVRO]"
+				+ "SET [LIV_NM_TITULO] ='" + titulo + "',[LIV_NM_AUTOR] = '"
+				+ autor + "',[LIV_SC_AREA] = '" + area + "',[LIV_NU_ANO] = '"
+				+ ano + "' WHERE LIV_NM_ISBN = '" + isbn + "'";
+
+		this.executarNonQuery(sql);
+	}
+
+	public ResultSet buscar(Livro livro) {
+		String isbn = livro.getIsbn();
+		ResultSet retorno = null;
+
+		String sql = "SELECT [LIV_ID],[LIV_NM_TITULO],[LIV_NM_AUTOR]"
+				+ ",[LIV_SC_AREA],[LIV_NU_ANO],[LIV_NM_ISBN]"
+				+ "FROM [biblioteca].[dbo].[LIV_LIVRO]"
+				+ "WHERE LIV_NM_ISBN = '" + isbn + "'";
+
+		conexao.abrirConexao();
+		retorno = conexao.executarQuery(sql);
+		conexao.fecharConexao();
+
+		return retorno;
+	}
+}
